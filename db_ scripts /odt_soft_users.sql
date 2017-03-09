b327d9351890debc3ce539f78e6357c98524d1ac
Aidenchii <diego.rozsanyi@gmail.com>
2017-03-09 22:32:51 +0000

    Alteracao da tabela users e adicao do script de criacao do banco de dados odt_soft

--- db_ scripts /odt_soft_users.sql
+++ db_ scripts /odt_soft_users.sql
@@ -19,9 +19,16 @@
 -- Table structure for table `users`
 --
 
-DROP TABLE IF EXISTS `users`;
+
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
+
+CREATE DATABASE odt_soft;	
+
+use odt_soft;	
+
+DROP TABLE IF EXISTS `users`;	
+
 CREATE TABLE `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
