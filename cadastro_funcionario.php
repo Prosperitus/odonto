9e71b4371c69e2db73470026d94bd06eb59eccf1
maxstur <futuromaxs@gmail.com>
2017-03-17 22:05:47 +0000

    criação do footer e definição da cor de fundo

--- cadastro_funcionario.php
+++ cadastro_funcionario.php
@@ -12,10 +12,25 @@
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
     <script type="text/javascript" src="js/materialize.min.js"></script>
     <script src="js/jquery.maskedinput.js"></script>
+<!--footer-->
+    <style>
+    body {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+    }
+
+    .main {
+    flex: 1 0 auto;
+    }
+
+    </style>
+    
+
 	
   </head>
 
-  <body>
+  <body class = "light-blue lighten-5">
     <!--CABEÇALHO-->
     <nav>
       <div class="nav-wrapper light-blue">
@@ -196,7 +211,7 @@
           </div>
 
           <!--CHECK-->
-            <div class="input-field col s12">
+            <div class="input-field col s12 main">
               <button class="btn waves-effect waves-light light-blue" type="submit" name="action">Confirmar Cadastro
                 <i class="material-icons right">send</i>
               </button>
@@ -216,5 +231,32 @@
 
     </script>
     <script src="js/mask_funcionario.js"></script>
-  </body>
+            <!--FOOTER-->
+        <footer class="page-footer light-blue">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Footer Content</h5>
+                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+                <h5 class="white-text">Links</h5>
+                <ul>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
+                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container">
+            © 2014 Copyright Text
+            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+            </div>
+          </div>
+        </footer>
+	
+ </body>
 </html>
