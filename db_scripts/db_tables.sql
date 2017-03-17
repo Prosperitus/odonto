52ca5c3e2d5b7e0204dfd3bd297e7a5a66dd47b1
TalesD <miletotg@gmail.com>
2017-03-17 18:05:56 -0300

    Atualizando nomes colunas

--- db_scripts/db_tables.sql
+++ db_scripts/db_tables.sql
@@ -12,49 +12,47 @@ CREATE TABLE 'users' (
   'surname' varchar(255) NOT NULL,
   'cro' varchar(10) NOT NULL,
   'email'varchar(255) NOT NULL,
-  'matricula' int (11) NOT NULL,  
+  'registration' int (11) NOT NULL,  
   'phone' int (11) NOT NULL,
   'address' varchar(255) NOT NULL,
   'admission_date' date NOT NULL,
   'password' varchar(100) NOT NULL,
-  'cpf' varchar (50) NOT NULL,
+  'social_security' varchar (50) NOT NULL,
   'bank' varchar (255) NOT NULL,
   'number_of_account' varchar (7) NOT NULL,
   'agency' int (11) NOT NULL,
   'permition' varchar (255) NOT NULL,
   'birthdate' date NOT NULL,	
  	
-	ADD CONSTRAINT "fk_permition" FOREIGN KEY ('permition') REFERENCES permition ('tipo');
+	ADD CONSTRAINT "fk_permition" FOREIGN KEY ('permition') REFERENCES permition ('type');
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
 
 DROP TABLE IF EXISTS 'permition';
 
 CREATE TABLE 'permition'(
 	'id' int(11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	'tipo' varchar(255) NOT NULL
+	'type' varchar(255) NOT NULL
 ) ENGINE=InnoDB AUTO_INCREMENT=1;
 
 
-DROP TABLE IF EXISTS 'paciente';
+DROP TABLE IF EXISTS 'patient';
 
-CREATE TABLE 'paciente'(
+CREATE TABLE 'patient'(
 	'id' int (255) NOT NULL AUTO_INCREMENT PRIMARY KEY, 
 	'name' varchar (255) NOT NULL,
-	'CPF' varchar (255) NOT NULL,
+	'social_security' varchar (255) NOT NULL,
 	'birthdate' varchar (255) NOT NULL,
 	'gender' varchar (10) NOT NULL,
 	'address' varchar(255) NOT NULL, 
-	'bairro' varchar(255) NOT NULL,
+	'neighborhood' varchar(255) NOT NULL,
 	'city' varchar(255) NOT NULL,
-	'UF' varchar(255) NOT NULL,
-	'CEP' varchar(255) NOT NULL,
-	'hospital' varchar(255) NOT NULL,
-	'plano_de_saude' varchar(255) NOT NULL,
-	'responsable1' varchar(255),
+	'state' varchar(255) NOT NULL,
+	'zip_code' varchar(255) NOT NULL,
+	'health_insurance' varchar(255) NOT NULL,
+	'responsible1' varchar(255),
 	'telephone_r1' varchar(255),
-	'prontuario' varchar(255) NOT NULL,
-	'clinica' varchar(255) NOT NULL,            
-	'responsable2' varchar(255),
+	'clinic' varchar(255) NOT NULL,            
+	'responsible2' varchar(255),
 	'telephone_r2' varchar(255),	
 	) 
 	ENGINE=InnoDB AUTO_INCREMENT=1;
