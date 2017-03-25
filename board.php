434901898e2ab04cfadab1fd013d85ced180eaec
Carlos Leonardo M. Lago <carlosleonardo.df@gmail.com>
2017-03-24 21:50:09 -0300

    modicacao do board.php

--- board.php
+++ board.php
@@ -37,55 +37,66 @@
       </div>
     </nav>
 
-    <!--CARD PACIENTE-->
-  <div class="row main">
-    <form class="col s6 m12">
-      
-      <div class="">
-        <div class="card">
-          <div class="card-image">
-            <img src="images/paciente.jpg">
-            <span class="card-title">Paciente</span>
-          </div>
-          <div class="card-content ">
-            <a href="cadastro_paciente.php" class="waves-effect waves-light btn-flat">Cadastrar</a>     
-            </div>
-          </div>
+
+    <!--Card Paciente---->
+
+ <!--OPCOES 2-->
+ <div class="row main">
+    <div class="col s6 m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/medico.jpg">
+          <span class="card-title"></span>
+          <a href="cadastro_funcionario.php" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
         </div>
+        <div class="card-content">
+          <p>Cadastro de Funcionario</p>
+        </div>
+      </div>
+    </div>
+  
+
+
       
         <!--CARD FUNCIONÁRIO-->
-        <div class="">
-          <div class="card">
-            <div class="card-image">      
-              <img src="images/medico.jpg">
-              <span class="card-title">Funcionário</span> 
-            </div>
-            <div class="card-content ">
-              <a href="cadastro_funcionario.php" class="waves-effect waves-light btn-flat">Cadastrar</a>
-            </div>
-          </div>
+        
+    <div class="col s6 m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/paciente.jpg">
+          <span class="card-title"></span>
+          <a href="cadastro_paciente.php" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
         </div>
-          
-        <!--CARD HOSPITAL-->
-        <div class="">
-          <div class="card">
-            <div class="card-image">      
-              <img src="images/hospital.jpg">
-              <span class="card-title">Hospital</span> 
-            </div>
-            <div class="card-content ">
-              <a href="cadastro_hospital.php" class="waves-effect waves-light btn-flat">Cadastrar</a>
-            </div>
-          </div>
+        <div class="card-content">
+          <p>Cadastro de Paciente</p>
         </div>
-      </form>
+      </div>
     </div>
+  
+
+      <!--CARD HOSPITAL-->
+    
+    <div class="col s6 m4">
+      <div class="card">
+        <div class="card-image">
+          <img src="images/hospital 2.jpg">
+          <span class="card-title"></span>
+          <a href="cadastro_hospital.php" class="btn-floating halfway-fab waves-effect waves-light red"><i class="material-icons">add</i></a>
+        </div>
+        <div class="card-content">
+          <p>Cadastro de Hospital</p>
+        </div>
+      </div>
+    </div>
+  </div>
+          
+        
   
       <!--FOOTER-->
         <footer class="page-footer light-blue">
           <div class="container">
             <div class="row">
-              <div class="col l6 s12">
+              <div class="col s6 s12">
                 <h5 class="white-text">Footer Content</h5>
                 <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
               </div>
