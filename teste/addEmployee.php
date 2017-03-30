2b032fc2d21caffcb795fdf43b9960c8078cefc5
Ighor <ighormac@gmail.com>
2017-03-30 19:36:17 -0300

    Atualizamos o codigo de teste e executamos

--- teste/addEmployee.php
+++ teste/addEmployee.php
@@ -6,64 +6,84 @@
 
 	function addEmployee(){
 		$Employee = new Employee();
+		$erro = false;
 
-		$name=$_POST["name"]);
+		$name = isset($_POST["funcionario_nome"]) ? $_POST["funcionario_nome"] : 0;
 		if(InvalidName($name)){
-			return "Nome Inválido";
+			echo "Nome Inválido</br>";
+			$erro = true;
 		}else{
-			$Employee->setName($_POST["name"]);
+			$Employee->setName($_POST["funcionario_nome"]);
 		}
 
-		$surname=$_POST["surname"]);
+		$surname=isset($_POST["funcionario_sobrenome"]) ? $_POST["funcionario_sobrenome"] : 0;
 		if(InvalidSurname($surname)){
-			return "Sobrenome Inválido";
+			echo "Sobrenome Inválido</br>";
+			$erro = true;
 		}else{
-			$Employee->setSurname($_POST["surname"]);
+			$Employee->setSurname($_POST["funcionario_sobrenome"]);
 		}
 
-		$cro=$_POST["cro"]);
-		if(letra_na_str($cro) || carac_na_str($cro)){
-			return "CRO Inválido";
-		}else{
-			$Employee->setCro($_POST["cro"]);
-		}
-
-		$admdata=$_POST["admdate"]);
-		if(InvalidAdmData($admdata)){
-			return "Data Inválida";
-		}else{
-			$Employee->setDate($_POST["admdate"]);
-		}
 		
-		$Employee->setNmat($_POST["nregistration"]);
-		
-		$cpf=$_POST["cpf"]);
-		if(InvalidCpf($cpf)){
-			return "CPF Inválido";
-		}else{
-			$Employee->setCpf($_POST["cpf"]);	
-		}
-		
-		$tel=$_POST["phone"]);
-		if(InvalidPhone($tel)){
-			return "Telefone Inválido";
-		}else{
-			$Employee->setPhone($_POST["phone"]);
-		}
-		
+		$Employee->setCro($_POST["funcionario_cro"]);
+		$Employee->setDate($_POST["funcionario_admissao"]);
+		$Employee->setRegistration(generateRegistry(6));
+		$Employee->setCpf($_POST["funcionario_cpf"]);	
+		$Employee->setPhone2($_POST["funcionario_celular"]);
+		$Employee->setPhone($_POST["funcionario_telefone"]);
 		$Employee->setAdress($_POST["adress"]);
-		$Employee->setBank($_POST["bank"]);
+		
+		$banco = isset($_POST["bank"]) ? $_POST["bank"] : 0;
+		if(InvalidName($banco)){
+			echo "Banco Inválido</br>";
+			$erro = true;
+		}else{
+			$Employee->setBank($_POST["bank"]);
+		}
+		
 		$Employee->setPermission($_POST["permissiontype"]);
 
-		$senha=$_POST["pass"]);
+		$senha=isset($_POST["funcionario_senha"]) ?$_POST["funcionario_senha"] :  0;
 		if(InvalidPassword($senha)){
-			return "Senha Inválida";
+			echo "Senha Inválida</br>";
+			$erro = true;
 		}else{
-			$Employee->setPass($_POST["pass"]);
+			$Employee->setPass(hash("sha256",($_POST["funcionario_senha"])));
 		}
 
+		$email = isset($_POST["funcionario_email"]) ? $_POST["funcionario_email"] :  0;
+		if(InvalidEmail($email)){
+			echo "Email Inválido</br>";
+			$erro = true;
+		}
+		else{
+			$Employee->setEmail($_POST["funcionario_email"]);	
+		}
+		
+		$cc = isset($_POST["funcionario_conta_banco"]) ? $_POST["funcionario_conta_banco"] : 0;
+		if(!letra_na_str($cc) || !carac_na_str($cc)){
+			echo "Cc Inválida</br>";
+			$erro = true;
+		}else{
+			$Employee->setNumberOfAccount($_POST["funcionario_conta_banco"]);
+		}
+
+		$ag = isset($_POST["funcionario_agencia_banco"]) ? $_POST["funcionario_agencia_banco"] :  0;
+		if(!letra_na_str($ag) || !carac_na_str($ag)){
+			echo "Ag Inválida</br>";
+			$erro = true;
+		}else{
+			$Employee->setAgency($_POST["funcionario_agencia_banco"]);
+		}
+
+
+
+
+
+		if(!$erro){
 		$conn = new DBconnect();
 		$conn->addEmployee($Employee);
+		}
 	}
 
 addEmployee();
--- teste/addPatient.php
+++ teste/addPatient.php
@@ -1,69 +1,102 @@
 <?php
 	include "patientModel.php";
 	include "../database/dbconnect.php";
+	include "funcoes.php";
+
+
+
 	function addPatient(){
 		$patient = new Patient();
-		
+		$erro = false;
+		$cpf = $_POST['cpf'] ? $_POST['cpf'] : 0;
+		$telefone_responsavel = $_POST['telefone_responsavel'] ? $_POST['telefone_responsavel'] : 0;
+		$nome_responsavel = $_POST['nome_responsavel'] ? $_POST['nome_responsavel'] : 0;
+		$cep_paciente = $_POST['cep_paciente'] ? $_POST['cep_paciente'] : 0;
+		$data_paciente = $_POST['date_paciente'] ? $_POST['date_paciente'] : 0;	
+		$sobrenome_paciente = $_POST['sobrenome_paciente'] ? $_POST['sobrenome_paciente'] : 0;
 		$nome_paciente = $_POST['nome_paciente'] ? $_POST['nome_paciente'] : 0;
+		
+
+		echo "nome_paciente : $nome_paciente</br>";
+		echo "sobrenome_paciente : $sobrenome_paciente</br>";
+		echo "data_paciente : $data_paciente</br>";
+		echo "cep_paciente : $cep_paciente</br>";
+		echo "nome_responsavel : $nome_responsavel</br>";	
+		echo "telefone_responsavel : $telefone_responsavel</br>";
+		echo "cpf : $cpf</br>";	
+		echo "------------------------------------------------------------------</br>";
+		echo "Erros : </br></br>";
+
+
 		if(!InvalidName($nome_paciente)){
 			$patient->setName($_POST['nome_paciente']);
 		}
 		else{
-			return "O nome do paciente esta invalido ";
+			$erro = true;
+			echo "Nome do paciente invalido</br>";
 		}
-		$sobrenome_paciente = $_POST['sobrenome_paciente'] ? $_POST['sobrenome_paciente'] : 0;
+		
 		if(!InvalidName($sobrenome_paciente)){
 			$patient->setSurname($_POST["sobrenome_paciente"]);
 		}
 		else{
-			return "O sobrenome do paciente esta invalido ";
+			$erro = true;
+			echo "Sobrenome do paciente invalido</br>";
 		}
 		
-		$patient->setSex($_POST['genero_paciente']);
-		$patient->setAddress($_POST['endereco_paciente']);
-		$patient->setNeighborhood($_POST['bairro_paciente']);
-		$patient->setCity($_POST['cidade_paciente']);
-		
-		$data_paciente = $_POST['date_paciente'] ? $_POST['date_paciente'] : 0;	
-		if(!InvalidData($date_paciente)){
+		if(!InvalidData($data_paciente)){
 			$patient->setBirthdate($_POST['date_paciente']);
 		}
 		else{
-			return "A data de nascimento esta invalida ";
+			$erro = true;
+			echo "A data de nascimento esta invalida</br>";
 		}		
-
-		$patient->setState($_POST['paciente_uf']);
-
-		$cep_paciente = $_POST['cep_paciente'] ? $_POST['cep_paciente'] : 0;	
-		if(!InvalidCEP($cep_paciente){
+	
+		if(!InvalidCEP($cep_paciente)){
 			$patient->setCep($_POST['cep_paciente']);
 		}
 		else{
-			return "O CEP do paciente esta invalido ";
+			$erro = true;
+			echo "O CEP do paciente esta invalido</br>";
 		}
 		
-		if(isset($_POST['social_security'])){
-			$patient->setSocialSecurity($_POST['social_security']);
-		}
-		$patient->setHealthPlan($_POST['plano_de_saude']);
-		
-		$nome_responsavel = $_POST['nome_responsavel'] ? $_POST['nome_responsavel'] : 0;	
-		if(!InvalidName($nome_responsavel){
+		if(!InvalidName($nome_responsavel)){
 			$patient->setResponsibleName($_POST['nome_responsavel']);
 		}
 		else{
-			return "O nome do responsavel esta invalido ";
+			$erro = true;
+			echo "O nome do responsavel esta invalido</br>";
 		}
-		$telefone_responsavel = $_POST['telefone_responsavel'] ? $_POST['telefone_responsavel'] : 0;
+			
 		if(!letra_na_str($telefone_responsavel) && !carac_na_str($telefone_responsavel)){
 			$patient->setResponsiblePhone($_POST['telefone_responsavel']);
 		}
 		else{
-			return "O telefone do responsavel esta invalido ";
+			$erro = true;
+			echo "O telefone do responsavel esta invalido</br>";
 		}
+			
+		if(!InvalidCpf($cpf)){
+			$patient->setSocialSecurity($_POST['cpf']);
+		}
+		else{
+			$erro = true;
+			echo "O cpf do paciente esta invalido</br>";
+		}	
+
+		$patient->setGender($_POST['genero_paciente']);
+		$patient->setAddress($_POST['endereco_paciente']);
+		$patient->setNeighborhood($_POST['bairro_paciente']);
+		$patient->setCity($_POST['cidade_paciente']);
+		$patient->setState($_POST['paciente_uf']);
 		$patient->setClinic($_POST['clinica']);
+		$patient->setHealthPlan($_POST['plano_de_saude']);
+
+		if(!$erro){
 		$conn = new DBconnector();
-		$conn->addPatient($patient);
-     
+		$result = $conn->addPatient($patient);
+		return $result;
+     	}
 	}
-addPatient();
+
+	addPatient();
