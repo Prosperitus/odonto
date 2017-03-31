c2f5d2bde56b2b74316e70c09c40089dbb93a0df
ArthurCamargo <arthurcamargo13@gmail.com>
2017-03-31 18:52:43 -0300

    Select Hospital

--- db_scripts/select.sql
+++ db_scripts/select.sql
@@ -1,3 +1,4 @@
+#substituir ? pela variavel
 SELECT `users`.`id`,
    `users`.`name`,
     `users`.`surname`,
@@ -17,4 +18,19 @@ SELECT `users`.`id`,
 
 FROM `odt_soft`.`users`
 WHERE users.name LIKE '%?%' OR surname LIKE '%?%' OR social_security = ? OR permition = ? OR registration = ? OR email like '%?%' OR cro = ?;
+
+
+
+SELECT `hospital`.`id`,
+    `hospital`.`name`,
+    `hospital`.`number_itu`,
+    `hospital`.`number_bed`,
+    `hospital`.`name_admin_itu`,
+    `hospital`.`telephone_admin_itu`,
+    `hospital`.`telephone_hospital`
+FROM odt_soft.hospital WHERE  `hospital`.`name` LIKE '%s%' 
+OR `hospital`.`name_admin_itu` LIKE '%?%' OR `hospital`.`number_itu` = ?;
+
+
+
 
