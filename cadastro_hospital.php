4edafdf336312e00d5825189b3c427945f1bc65a
andre951 <andre.vb.cruz@outlook.com>
2017-04-06 21:30:26 +0000

    navbar e pesquisa em todas as telas

--- cadastro_hospital.php
+++ cadastro_hospital.php
@@ -20,22 +20,70 @@
 
   <body class = "light-blue lighten-5">
 
-    <!--CABEÇALHO-->
-    <div class="navbar-fixed">
-      <nav>
-        <div class="nav-wrapper light-blue">
-          <ul id="nav-mobile" class="left">
-            <li><a href="board.php">Home</a></li>
-            <li><a href="cadastro_funcionario.php">Cadastro Funcionário</a></li>
-            <li><a href="cadastro_paciente.php">Cadastro Paciente</a></li>
+   <!--Scrip sidenav-->
 
-          </ul>
-          <ul id="nav-mobile" class="right">
-            <li><a href="backend/logout.php">Logout</a></li>
-          </ul>
-        </div>
-      </nav>
+
+<script type="text/javascript">
+(function($){
+    $(function(){
+        $('select').material_select();
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+    }); // end of document ready
+})(jQuery); // end of jQuery name space</script>
+
+
+    <!--CABEÇALHO-->
+   <nav>
+  <div class="nav-wrapper light-blue">
+  <div class="col s2">
+        <ul class="left">
+          <a href="#" data-activates="slide-out" class="button-collapse show-on-large"><i class="material-icons">menu</i></a>
+        </ul>
+      </div>
+    <div class="row">
+      <div class="col s10">
+       <ul class="right">
+        <form>
+          <div class="input-field">
+            <input id="search" type="search" />
+            <label for="search"><i class="material-icons">search</i></label>
+            </ul>
+          </div>
+        </form>
+      </div>
     </div>
+  </div>
+</nav>
+
+ <!--Sidenav-->
+
+<div class="userView position:relative">
+  <ul id="slide-out" class="side-nav ">
+    <li><div class="userView">
+      <div class="background blue">
+
+      </div>
+      <a href="#!user"><img class="circle" src="images/medico.jpg"></a>
+      <a href="#!name"><span class="white-text name">Bem Vindo "John Doe"</span></a>
+      <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
+    </div></li>
+    <li><a href="#!"><i class="material-icons">note_add</i>Cadastrar</a></li>
+    <li><a href="#!"><i class="material-icons">add_alert</i>Hospitais</a></li>
+    <li><a href="#!"><i class="material-icons">perm_identity</i>Pacientes</a></li>
+    <li><a href="#!"><i class="material-icons">cloud</i>Arquivos</a></li>
+    
+    <li><div class="divider"></div></li>
+    <li><a class="subheader">Mais opçoes</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+  </ul>
+  </div>
+
 
     <!--CLASS/FORM-->
     <div class="row">
