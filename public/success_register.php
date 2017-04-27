e597c3238975b1c559c84ad4d8709691480b4e3d
joaocarlos13 <joaolossio13@gmail.com>
2017-04-27 19:35:12 -0300

    Melhorada a interface do sucesso de cadastro

--- public/success_register.php
+++ public/success_register.php
@@ -1,7 +1,27 @@
 <?php 
 	require_once "cabecalho.php";
 ?>
-	<div class="main">
-	<p>Cadastro efetuado com sucesso!</p>
-	</div>
+
+
+
+  <div class="container main">
+  <div class="row">
+    <div class="col s2">&nbsp;</div>
+    <div class="col s6">
+      <div class="card">
+        <div class="card-image">
+          <img src="../images/checked.jpg">
+          <span class="card-title">Cadastro realizado com sucesso</span>           
+        </div>     
+        <div class="card-content">
+        </div>
+        <div class="card-action">
+          <a href="board.php">Tela Principal</a>
+          <a href='cadastro_funcionario.php'>Cadastrar Novamente</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
  <?php require_once "rodape.php";
