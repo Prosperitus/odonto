1e325006f106f8ea2758caf7edce6b697f1f8556
lucas616 <lucas.barbosa.almeida@outlook.com>
2017-04-27 21:59:41 -0300

    implimentamento do codigo busca hospital e atualizacao do codigo model hospital

--- backend/Hospitalmodel.php
+++ backend/Hospitalmodel.php
@@ -1,98 +1,47 @@
 <?php
 
 class Hospital{
-	
 
-	private $namehosp;
-	private $chefuti;
-		//private $phonechef;
-		//private $phonehosp;
+		private $namehosp;
+		private $chefuti;
+		private $phonechef;
+		private $phoneUti;
 
-	function __construct(){
+		function __construct(){
 
-	}		
+		}		
 
 
-	public function getNameHosp(){
-		return $this->namehosp;
-	}
-	
+		public function getNameHosp(){
+			return $this->namehosp;
+		}
+		
+		public function setNameHosp($namehosp){
+			$this->name = $namehosp;
+		}
 
-	public function setNameHosp($namehosp){
-		$this->name = $namehosp;
-	}
 
-	public function getTelephonehosp(){
-		return $this->telephonehosp;
-	}
+		public function getChefUti(){
+			return $this->chefuti;
+		}
+		
+		public function setChefUti($chefuti){
+			$this->name = $chefuti;
+		}
 
-	public function setTelephonehosp($telephonehosp){
-		return $this->telephonehosp;
-	}
+		public function getPhoneChef(){
+			return $this->phonechef;
+		}
+		
+		public function setPhoneChef($phonechef){
+			$this->name = $phonechef;
+		}
 
-	
-	public function getFiles(){
-		return $this->files;
-	}
+		public function getPhoneUti(){
+			return $this->phoneuti;
+		}
+		
+		public function setPhoneUti($phoneuti){
+			$this->name = $phoneuti;
+		}
 
-	public function setFiles($files){
-		return $this->files;
-	}
-	
-	public function getName_Itu(){
-		return $this->name_itu;
-	}
-	
-	public function setName_itu($name_itu){
-		return $this->name_itu;
-	}
-
-	public function getName_bed(){
-		return $this->name_bed;
-	}
-
-	public function setName_bed($name_bed){
-		return $this->name_bed;
-	}
-
-	public function getNum_itu(){
-		return $this->num_itu;
-	}
-
-	public function setNum_itu($num_itu){
-		return $this->num_itu;
-	}
-
-	public function getNum_bed(){
-		return $this->num_bed;
-	}
-
-	public function setNum_bed($num_bed){
-		return $this->num_bed;
-	}
-	public function getName_admin(){
-		return $this->name_admin;
-	}
-
-	public function setName_admin($name_admin){
-		return $this->name_admin;
-	}
-
-	public function getTelephone_admin_itu(){
-		return $this->telephone_admin_itu;
-	}
-
-	public function setTelephone_admin_itu($telephone_admin_itu){
-		return $this->telephone_admin_itu;
-	}
-
-	public function getHospital(){
-		return $this->hospital;
-	}
-
-	public function setHospital($hospital){
-		return $this->hospital;
-	}
-
-	
-}
