c3db4652c922ff8b595169078438ba72eb0397d4
joaocarlos <joaolossio13@gmail.com>
2017-05-12 18:45:06 -0300

    rodape atualizado

--- public/rodape.php
+++ public/rodape.php
@@ -1,26 +1,14 @@
   <!--FOOTER-->
         <footer class="page-footer light-blue">
-          <div class="container ; position:absolute">
-            <div class="row">
-              <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-              </div>
-              <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                  <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-              </div>
-            </div>
-          </div>
           <div class="footer-copyright">
             <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                <a  class="grey-text text-lighten-4 left" href=""><b>Contato &nbsp;&nbsp;</b></a>
+                <a class="grey-text text-lighten-4 left" href=""><b>Facebook &nbsp;&nbsp;</b></a>
+                <a class="grey-text text-lighten-4" left href=""><b>Instagram &nbsp;&nbsp;</b></a>
+                 <a class="grey-text text-lighten-4 left" href=""><b>Manual &nbsp;&nbsp;</b></a>
+                <a class="grey-text text-lighten-4" left href=""><b>Email &nbsp;&nbsp;</b></a>
+              
+            <a class="grey-text text-lighten-4 right" href="#!">© 2017 easyodonto solutions® - Todos os direitos reservados</a>
             </div>
           </div>
         </footer>
