dc00e435793d504a1912a8a1c59e4dae8842ed83
SolutionB <rafa3.3.3@hotmail.com>
2017-05-19 17:06:40 -0300

    adicionando letras maiusculas ao email

---
+++ backend/patientModel2.php
@@ -0,0 +1,199 @@
+<?php
+	
+class Patient{
+
+	private $name;
+	private $surname;
+	private $birthdate;
+	private $gender;
+	private $adress;
+	private $neighborhood;
+	private $city;
+	private $state;
+	private $cep;
+	private $hospital;
+	private $healthPlan;
+	private $responsibleName;
+	private $responsiblePhone;
+	private $medicalRecords;
+	private $clinic;
+	private $medicalassistant;
+	private $cpf;
+	private $pacienteEmail;
+	private $responsibleName2;
+	private $responsiblePhone2;
+	
+
+	public function __construct(){
+
+	}
+
+	public function getName(){
+		return $this->name;
+	}
+
+	public function setName($name){
+		$this->name = $name;
+	}
+
+	public function getSurname(){
+		return $this->surname;
+	}
+
+	public function setSurname($surname){
+		$this->surname = $surname;
+	}
+
+	public function getBirthdate(){
+		return $this->birthdate;
+	}
+
+	public function setbirthdate($birthdate){
+		$this->birthdate = $birthdate;
+	}
+	
+	public function getGender(){
+		return $this->gender;
+	}
+
+	public function setGender($Gender){
+		$this->gender = $Gender;
+	}
+
+	public function getAdress(){
+		return $this->Adress;
+	}
+
+        public function setAdress($Adress){
+		$this->Adress = $Adress;
+         
+        }
+
+	
+	public function getNeighborhood(){
+		return $this->neighborhood;
+	}
+
+	public function setNeighborhood($neighborhood){
+		$this->neighborhood = $neighborhood;
+	}
+
+	public function getState(){
+		return $this->state;
+	}
+
+	public function setState($state){
+		$this->state = $state;
+	}
+
+	public function getCity(){
+		return $this->city;
+	}
+
+	public function setCity($city){
+		$this->city = $city;
+	}
+
+
+	public function getCep(){
+		return $this->cep;
+	}
+
+	public function setCep($cep){
+		$this->cep = $cep;
+	}
+	
+	public function getHospital(){
+		return $this->hospital;
+	}
+
+	public function setHospital($hospital){
+		$this->hospital = $hospital;
+	}
+
+	public function getHealthPlan(){
+		return $this->healthPlan;
+	}
+
+	public function setHealthPlan($healthPlan){
+		$this->healthPlan = $healthPlan;
+	}
+
+	public function getResponsibleName(){
+		return $this->responsibleName;
+	}
+
+	public function setResponsibleName($responsibleName){
+		$this->responsibleName = $responsibleName;
+	}
+
+	public function getResponsiblePhone(){
+		return $this->responsiblePhone;
+	}
+
+	public function setResponsiblePhone($responsiblePhone){
+		$this->responsiblePhone = $responsiblePhone;
+	}
+	
+	public function getMedicalRecords(){
+		return $this->medicalRecords;
+	}
+
+	public function setMedicalRecords($medicalRecords){
+		$this->medicalRecords = $medicalRecords;
+	}
+
+	public function getClinic(){
+		return $this->clinic;
+	}
+
+	public function setClinic($clinic){
+		$this->clinic = $clinic;
+	}
+	
+	public function getCpf(){
+		return $this->cpf;
+	}
+
+	public function setCpf($cpf){
+		$this->cpf = $cpf;
+	}
+	
+        public function getmedicalassistant(){
+               return $this ->medicalassistant;
+	}
+	
+        public function setmedicalassistant($medicalassistant){
+               $this->medicalassistant =$medicalassistant;        
+        }
+        
+        public function getpacienteEmail(){
+               return $this->pacienteEmail;  
+        }
+        public function setpacienteEmail($pacienteEmail){
+	        $this->pacienteEmail =$pacienteEmail;
+   
+  
+     }
+
+		//NOVOS CAMPOS
+	
+	
+
+
+	public function getResponsiblePhone2(){
+		return $this->responsiblePhone2;
+	}
+
+	public function setResponsiblePhone2($responsiblePhone2){
+		$this->responsiblePhone2 = $responsiblePhone2;
+	}
+
+	public function getResponsibleName2(){
+		return $this->responsibleName2;
+	}
+
+	public function setResponsibleName2($responsibleName2){
+		$this->responsibleName2 = $responsibleName2;
+	}
+}
--- public/cadastro_funcionario.php
+++ public/cadastro_funcionario.php
@@ -163,7 +163,7 @@
         <!--EMAIL-->
         <div class="input-field col s5">
           <i class="material-icons prefix">email</i>
-          <input id="funcionario_email" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,10}$" class="validate" name="funcionario_email" required>
+          <input id="funcionario_email" type="email" pattern="[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,10}$" class="validate" name="funcionario_email" required>
           <label for="funcionario_email">Email</label>
         </div>
 
