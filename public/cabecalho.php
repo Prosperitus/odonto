3a81e2386e8b6c7748d89af4e622f38b2e79d401
Pedro <pedro.bomfim03@gmail.com>
2017-05-25 18:20:19 -0300

    atualizacao

--- public/cabecalho.php
+++ public/cabecalho.php
@@ -10,7 +10,6 @@ $user = userInform();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="utf-8"/>
     <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
-    <script type="text/javascript" href="../js/jquery.js"></script> 
     <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="../css/style.css"/>
     <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>      
@@ -59,6 +58,41 @@ $user = userInform();
           padding-bottom: 60px;
     		}
       }
+		.ui-button{
+			background-color: #0399ff;
+			color:white;
+			border-color:#0399ff;
+			font-weight:bold
+		}
+		.ui-button:hover{
+			background-color: white;
+			color:#0399ff;
+			border-color:#0399ff;
+			font-weight:bold
+		}
+		
+		.ui-dialog-titlebar-close
+		{
+		 position: absolute;
+		 right: .3em;
+		 top: 50%;
+		 width: 19px;
+		 margin: -10px 0 0 0;
+		 padding: 1px;
+		 height: 18px;
+
+		 /*Esconder o X*/
+		 display: none;
+		}
+		.ui-dialog-titlebar{
+			background-color: #0399ff;
+			color:white;
+			border-color:#0399ff;
+			font-weight:bold
+		}
+		
+		
+	
     </style>
 
 
