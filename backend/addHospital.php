9dde857abc1bcb7822f0923f9d9c72e03563ced9
David Pena <haruhashilives@gmail.com>
2017-05-26 21:39:47 -0300

    Alterações para adicionar arquivos no cadastro de hospital

--- backend/addHospital.php
+++ backend/addHospital.php
@@ -11,17 +11,36 @@
 		$Hospital->setPhoneChef($_POST["telefone_chefe_uti"]);
 		$Hospital->setPhoneUti($_POST["telefone_uti"]);
 		$conn = new DbHospital();
+		
 		$result = $conn->addHospital($Hospital);
 
-		if($result && isset($_FILES['imagemHospital']) && $_FILES['imagemHospital']['size'] > 0){
+		if($result &&  ( isset($_FILES['imagemHospital']) && $_FILES['imagemHospital']['size'] > 0 
+		|| isset($_FILES['file_upload']) && $_FILES['imagemHospital']['size'] > 0)){
+		
 			$id = $conn->search_max_id();
+			
+			$extensao_file = strtolower(strrchr($_FILES['file_upload']['name'],'.'));
 			$extensao = strtolower(strrchr($_FILES['imagemHospital']['name'],'.'));
+			
+			$file = $_FILES['file_upload']['name'];
 			$imagem = $_FILES['imagemHospital']['name'];
+			
+			$file = substr(hash("sha256",md5(uniqid(time()))),0,12).$extensao_file;
 			$imagem = substr(hash("sha256",date("Y-m-d H:i:s")),0,12).$extensao;
+			
 			$destino = '../images/hospital/' .$imagem;
+			$destino_file = '../file_upload/hospital/' .$file;
+			
 			$arquivo_tmp = $_FILES['imagemHospital']['tmp_name'];
+			$arquivo_tmp_file = $_FILES['file_upload']['tmp_name'];
+			
 			move_uploaded_file( $arquivo_tmp, $destino);
+			move_uploaded_file( $arquivo_tmp_file, $destino_file);
+			
 			$conn->addImagem($destino,$id);
+			$conn->addFile($destino_file,$id);
+			
+			
 		}
 		return $result;
 	}
