6960557c85b749d1e1106da7fe5b71cae0f2f370
joaoalmeida25 <joao_almeida25@hotmail.com>
2017-06-09 18:16:09 -0300

    Alteração envia_email

--- backend/envia_email.php
+++ backend/envia_email.php
@@ -3,7 +3,14 @@
     	<title>Envia Email</title>
 	    <meta charset="utf-8"/>
 	    <link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
-	 </head>
+	 	<style type="text/css">
+	 		.mensagem {
+	 			font-size: 50px;
+	 			margin-left: 430;
+	 			margin-top: 150;
+	 		}
+	 	</style>
+	</head>
   	<body>
   		<header>
 		    <nav>
@@ -11,29 +18,33 @@
 		      </div>
 		    </nav>
 	    </header>
-	    <div class="main margin_small">
-			<?php
-				$quebra_linha = "\n";
-				$envia_email = "contato@".$_SERVER['HTTP_HOST'];
-				$destinatario = $_POST['E_mail'];
-				$assunto = "Email teste";
-				$mensagem = "Funciona";
+	    <div class="mensagem">
+		    <div class="main margin_small">
+				<br/>
+				<?php
+					$quebra_linha = "\n";
+					$envia_email = "contato@".$_SERVER['HTTP_HOST'];
+					$destinatario = $_POST['E_mail'];
+					$assunto = "Email teste";
+					$mensagem = "Funciona";
 
-				$mensagemHTML = '<p> Teste de Email </p>
-				<p> Titulo </p>
-				<p><b><t>'.$mensagem.'</p></b></t>
-				<br>';
+					$mensagemHTML = '<p> Teste de Email </p>
+					<p> Titulo </p>
+					<p><b><t>'.$mensagem.'</p></b></t>
+					<br>';
 
-				$headers = "MIME-Version: 1.1".$quebra_linha;
-				$headers .= "Content-type: text/html; chaset = iso-8859-1".$quebra_linha;
-				$headers .= "From: ".$envia_email.$quebra_linha;
-				$headers .= "Return-Path: ".$envia_email.$quebra_linha;
+					$headers = "MIME-Version: 1.1".$quebra_linha;
+					$headers .= "Content-type: text/html; chaset = iso-8859-1".$quebra_linha;
+					$headers .= "From: ".$envia_email.$quebra_linha;
+					$headers .= "Return-Path: ".$envia_email.$quebra_linha;
 
-				mail($destinatario, $assunto, $mensagemHTML, $headers, "-r". $envia_email);
+					mail($destinatario, $assunto, $mensagemHTML, $headers, "-r". $envia_email);
 
-				print("Mensagem enviada!");
-				?>
+					print("Mensagem enviada!");
+					?>
+			</div>
 		</div>
+		<!-- <h6 style="margin-left: 400">Obs: Rodrigo não teve qualquer tipo de envolvimento na criação deste código.</h6>	-->
       	<footer class="page-footer light-blue" style="margin: auto; width: 100%; bottom: 0; position: fixed;">
 		    <div class="footer-copyright">
 		    	<div class="container">	
