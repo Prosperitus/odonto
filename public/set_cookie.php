e3faba4a40658543d9382f6bd8efcfc29698009c
IghorMRS <Ighormac@gmail.com>
2017-06-14 19:24:13 -0300

    atualizacao na funcao de manipulacao de cookie

--- public/set_cookie.php
+++ public/set_cookie.php
@@ -1,22 +1,8 @@
 <?php
  
- function setCookie(){
- 	$cookie_name = $_COOKIE['patient'];
- 	?>
- 	<html>
- 	<body>
- 	<?php
- 	$true = true;
- 	$false = false;	
-		setcookie("Paciente",$cookie_name, time()+(86400 * 30), "/");
- 	 
- 	 if(!isset($cookie_name)){
- 	 	return $false;
- 	 	}else{
- 	 		return $true;
- 	 		}
- 		
- 		?>
- 		</body>
- 		</html>
- 		}
+ function getCookiePatient(){
+ 	if(isset($_COOKIE['patient'])){
+ 	$patient = $_COOKIE['patient'];
+ 	 	return $patient->name;
+ 	}
+ }
