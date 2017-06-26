3488bc5765a6a033eb75edd75c506aa01261eedc
camilancardoso <cncardoso96@gmail.com>
2017-06-26 14:16:48 -0300

    Update searchGrafic.php

--- request/searchGrafic.php
+++ request/searchGrafic.php
@@ -2,10 +2,23 @@
 	require_once "../database/attendance.php";
 
 
+
 	$option = $_GET['grafic'];
 	if($option == '1'){
 		$db = new AttendanceDb();
 		$countStatus = $db->searchCountStatus();
+		$data = array('count' => [] , 'name' => []);
+		foreach($countStatus as $count){
+			array_push($data['count'],$count->count);
+			array_push($data['name'],$count->name);
+		}
+		$json = json_encode($data);
+		echo $json;
+	}
+
+	if($option == '2'){
+		$db = new AttendanceDb();
+		$countStatus = $db->searchCountAdmissionCause();
 		$data = array('count' => [] , 'name' => []);
 		foreach($countStatus as $count){
 			array_push($data['count'],$count->count);
