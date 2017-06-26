80bc0ce7a2c60a197dfa3ccd6613d0f46b972877
camilancardoso <cncardoso96@gmail.com>
2017-06-26 13:45:19 -0300

    Update graficos.php

--- public/graficos.php
+++ public/graficos.php
@@ -5,17 +5,20 @@
 
 <div class="input-field col s6" style="width: 300px;left:40%;margin_left:-150px">
     <select name="graficoSelect" id="graficoSelect">
-		<option value="n" selected>Quantidade de atendimentos por status</option>
+    	<option value="n" disabled selected>Selecione um tipo de gráfico</option>
+		<option value="1">Quantidade de atendimentos por status</option>
+		<option value="2">Condição Sistêmica por status</option>
 	</select>
     <label for="graficoSelect">Grafico</label>
 </div>
 <div id="grafico">
-	<div class="welcome">Quantidade de atendimentos por status</div>
+	<div class="welcome" id="welcomeGrafic"></div>
 	<div class="grafico center">
 		<canvas id="myChart" width="400" height="400"></canvas>
 	</div>
 	<script src="../js/chart.js"></script>
 </div>
+
 <?php
 	require_once "rodape.php";
 ?>
