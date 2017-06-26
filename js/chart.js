e9a6864c443774637409edb63a7647780a254b77
camilancardoso <cncardoso96@gmail.com>
2017-06-26 14:14:34 -0300

    Update chart.js

--- js/chart.js
+++ js/chart.js
@@ -1,7 +1,10 @@
 // SCRIPTS DE GRAFICOS
 
 $(document).ready(function(){
+	var myChart;
 	$('#graficoSelect').change(function(){
+		var canvas = document.getElementById("myChart");
+		var ctx = canvas.getContext('2d');
 		value = $('#graficoSelect').val();
 		$.ajax({
 			type: 'GET',
@@ -26,8 +29,13 @@ $(document).ready(function(){
 									'#B0E2FF'];
 					var dados = dataJson['count'];
 					var nomes = dataJson['name'];
-					var ctx = document.getElementById("myChart").getContext('2d');
-					var myChart = new Chart(ctx, {
+					$('graficContent').html('');
+					$('graficContent').html('<canvas id="myChart" width="400" height="400"></canvas>');
+					ctx.clearRect(0, 0, canvas.width, canvas.height);
+					if(myChart != null){
+						myChart.destroy();
+					}
+					myChart = new Chart(ctx, {
 						type: 'pie',
 						data: {
 							
@@ -58,6 +66,53 @@ $(document).ready(function(){
 							responsive: true
 						}
 					});
+				}
+
+				if(value == '2'){
+					$('#welcomeGrafic').html("Quantidade de atendimentos por status");
+					var dataJson = $.parseJSON(data);
+					var arrayColor = ['red',
+									'orange',
+									'yellow',
+									'green',
+									'blue',
+									'#00F5FF',
+									'#FF4040',
+									'#54FF9F'
+									];
+					var dados = dataJson['count'];
+					var nomes = dataJson['name'];
+					ctx.clearRect(0, 0, canvas.width, canvas.height);
+					if(myChart != null){
+						myChart.destroy();
+					}
+					myChart = new Chart(ctx, {
+						type: 'pie',
+						data: {
+							
+							labels: nomes,
+							datasets: [{
+								 labels: arrayColor,
+								data: dados,
+								backgroundColor: arrayColor,
+								borderColor: [
+									'white',
+									'white',
+									'white',
+									'white',
+									'white',
+									'white',
+									'white',
+									'white'	
+								],
+								borderWidth: 2
+							}]
+						},
+						options: {
+							responsive: true
+						}
+					});
+
 				}
 			}
 	    });
