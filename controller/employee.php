64243e0814cdd0a91cd3cfc1559d4c6323c573db
Rafaella <pinkrafaella@hotmail.com>
2017-06-29 13:12:31 -0300

    edição de funcionários atualizado

--- controller/employee.php
+++ controller/employee.php
@@ -26,21 +26,21 @@ class EmployeeController{
 	
 	public function add(){
 		$Employee = new Employee();
-		$Employee->setName($_POST["funcionario_nome"]);
-		$Employee->setSurname($_POST["funcionario_sobrenome"]);
-		$Employee->setCro($_POST["funcionario_cro"]);
+		$Employee->setName(filter_input(INPUT_POST, 'funcionario_nome'));
+		$Employee->setSurname(filter_input(INPUT_POST, 'funcionario_sobrenome'));
+		$Employee->setCro(filter_input(INPUT_POST,'funcionario_cro'));
 		$Employee->setRegistration(generator(6));
-		$Employee->setAdmissionDate($_POST["funcionario_admissao"]);
-		$Employee->setCpf($_POST["funcionario_cpf"]);
-		$Employee->setPhone($_POST["funcionario_telefone"]);
-		$Employee->setPhone2($_POST["funcionario_celular"]);
-		$Employee->setAddress($_POST["funcionario_endereco"]);
-		$Employee->setBank($_POST["funcionario_banco"]);
-		$Employee->setPermission($_POST["permissao"]);
-		$Employee->setPassword(hash("sha256",($_POST["funcionario_senha"])));
-		$Employee->setEmail($_POST["funcionario_email"]);
-		$Employee->setNumberOfAccount($_POST["funcionario_conta_banco"]);
-		$Employee->setAgency($_POST["funcionario_agencia_banco"]);
+		$Employee->setAdmissionDate(filter_input(INPUT_POST,'funcionario_admissao'));
+		$Employee->setCpf(filter_input(INPUT_POST, 'funcionario_cpf'));
+		$Employee->setPhone(filter_input(INPUT_POST, 'funcionario_telefone'));
+		$Employee->setPhone2(filter_input(INPUT_POST,'funcionario_celular'));
+		$Employee->setAddress(filter_input(INPUT_POST,'funcionario_endereco'));
+		$Employee->setBank(filter_input(INPUT_POST,'funcionario_banco'));
+		$Employee->setPermission(filter_input(INPUT_POST,'permissao'));
+		$Employee->setPassword(hash("sha256",(filter_input(INPUT_POST,'funcionario_senha'))));
+		$Employee->setEmail(filter_input(INPUT_POST, 'funcionario_email'));
+		$Employee->setNumberOfAccount(filter_input(INPUT_POST, 'funcionario_conta_banco'));
+		$Employee->setAgency(filter_input(INPUT_POST, 'funcionario_agencia_banco'));
 		$conn = new EmployeeDb();
 		$result = $conn->add($Employee);
 		$this->redirect($result);
